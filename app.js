456e7ce4465645b9051164de2416ce4182213e27
kaweechai22 <cacarosjampa@gmail.com>
2026-06-18 18:09:15 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const $=id=>document.getElementById(id);
-/* v5.226 standing wave particle pressure verified */
+/* v5.228 standing wave merged verified */
 const rebuiltTopicModes = new Set(["soundReflection","soundRefraction","soundDiffraction","soundInterference","superposition","beatsViz","standingAir","resonanceViz","resonanceAirHarmonics","dopplerViz","shockWave","soundIntensity","soundIntensityLevel","soundLevelHearing","harmonicsViz","noisePollution","applicationsSound"]);
 const soundTopicModes = rebuiltTopicModes;
 
